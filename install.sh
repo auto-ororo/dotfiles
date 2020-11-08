17ac95ded4720806f1c91f09547e041ff9c61f2b
Ryo Narisawa <ororoauto@gmail.com>
2020-11-08 17:41:38 +0900

    modify install.sh

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ for f in .??*
 do
     [[ "$f" == ".git" ]] && continue
     [[ "$f" == ".gitmodules" ]] && continue
+    [[ "$f" == ".gitignore" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
 
     ln -sf ~/dotfiles/$f ~/$f
