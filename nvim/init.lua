5b2a4141754ddc75b7280c081063589f836202a3
Ryo Narisawa <ororoauto@gmail.com>
2023-07-08 08:09:25 +0900

    シンタックスハイライトの対象言語を追加

--- nvim/init.lua
+++ nvim/init.lua
@@ -300,7 +300,7 @@ vim.keymap.set('n', '<C-e>', require('telescope').extensions.file_browser.file_b
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'kotlin', 'markdown', 'swift', 'dart', 'yaml' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
